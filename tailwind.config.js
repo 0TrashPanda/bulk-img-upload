28aa3f9e5593816be9042930ad676c484bf3505c
0TrashPanda <jonah.jvdb@gmail.com>
2023-11-14 03:21:04 +0100

    did some stuff
    Co-authored-by: TheTreeSee <TheTreeSee@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {}, 
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+    },
+    extend: {},
   },
   plugins: [],
 }
